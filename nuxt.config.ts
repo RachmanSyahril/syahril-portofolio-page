8dcf0aa9c5e64ea2add79f12eec8f9ecfe80a3b4
Syahril Rachman <rachmansyahril@gmail.com>
2026-04-29 12:46:36 +0700

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,21 @@ export default defineNuxtConfig({
         { rel: 'apple-touch-icon', href: 'icon-192.svg' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=JetBrains+Mono:wght@400;500&display=swap' }
+      ],
+      script: [
+        {
+          src: 'https://www.googletagmanager.com/gtag/js?id=G-DHH1LQQRRP',
+          async: true
+        },
+        {
+          innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DHH1LQQRRP');
+          `,
+          type: 'text/javascript'
+        }
       ]
     }
   },
